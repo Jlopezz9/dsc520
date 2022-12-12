98184fae00ad9ac213348426777f619168efe762
Jlopezz97 <112675855+Jlopezz97@users.noreply.github.com>
2022-12-11 19:33:29 -0800

    assignment01

---
+++ assignments/assignment00/assignment_00_LopezJulissa.R
@@ -0,0 +1,58 @@
+---
+output:
+  pdf_document: default
+  html_document: default
+---
+# Assignment: ASSIGNMENT 0
+# Name: Lopez, Julissa
+# Date: 2022-12-09
+# Basics
+
+## Add 8 and 5
+8+5 
+
+## Subtract 6 from 22
+22-6
+
+## Multiply 6 by 7
+6*7
+
+## Add 4 to 6 and divide the result by 2
+(4+6)/2
+
+## Compute 5 modulo 2
+5%%2
+
+## Assign the value 82 to the variable x
+## Print x
+x <- 82
+print(x)
+
+## Assign the value 41 to the variable y
+## Print y
+y <- 41
+print(y)
+
+## Assign the output of x + y to the variable z
+## Print z
+z <- x+y
+print(z)
+
+## Assign the string value "DSC520" to the variable class_name
+## Print the value of class_name
+class_name <- 'DSC520'
+print(class_name)
+
+## Assign the string value of TRUE to the variable is_good
+## Print the value of is_good
+is_good <- TRUE
+print(is_good)
+## Check the class of the variable is_good using the `class()` function
+class(is_good)
+
+## Check the class of the variable z using the `class()` function
+class(z)
+
+## Check the class of the variable class_name using the class() function
+
+tinytex::tlmgr_install(c("amscls", "amsfonts", "amsmath", "atbegshi", "atveryend", "auxhook", "babel", "bibtex", "bigintcalc", "bitset", "booktabs", "cm", "ctablestack", "dehyph", "dvipdfmx", "dvips", "ec", "epstopdf-pkg", "etex", "etexcmds", "etoolbox", "euenc", "everyshi", "fancyvrb", "filehook", "firstaid", "float", "fontspec", "framed", "geometry", "gettitlestring", "glyphlist", "graphics", "graphics-cfg", "graphics-def", "helvetic", "hycolor", "hyperref", "hyph-utf8", "hyphen-base", "iftex", "inconsolata", "infwarerr", "intcalc", "knuth-lib", "kpathsea", "kvdefinekeys", "kvoptions", "kvsetkeys", "l3backend", "l3kernel", "l3packages", "latex", "latex-amsmath-dev", "latex-bin", "latex-fonts", "latex-tools-dev", "latexconfig", "latexmk", "letltxmacro", "lm", "lm-math", "ltxcmds", "lua-alt-getopt", "lua-uni-algos", "luahbtex", "lualatex-math", "lualibs", "luaotfload", "luatex", "luatexbase", "mdwtools", "metafont", "mfware", "modes", "natbib", "path", "pdfescape", "pdftex", "pdftexcmds", "plain", "psnfss", "refcount", "rerunfilecheck", "scheme-infraonly", "selnolig", "stringenc", "symbol", "tex", "tex-ini-files", "texlive-scripts", "texlive.infra", "times", "tipa", "tlgs", "tlperl", "tools", "unicode-data", "unicode-math", "uniquecounter", "url", "xcolor", "xetex", "xetexconfig", "xkeyval", "xunicode", "zapfding"))
